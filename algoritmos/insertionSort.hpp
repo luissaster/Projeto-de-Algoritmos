11ed8c3949f787c9a62f4e25a77e9c6ff7b9f3a2
Luís Fernando Almeida <luisfernalme@gmail.com>
2023-09-03 17:15:48 -0300

    insertion sort

--- algoritmos/insertionSort.hpp
+++ algoritmos/insertionSort.hpp
@@ -9,15 +9,15 @@ std::vector<int> insertionSort(std::vector<int> &arr)
     int n = arr.size();
     for (int i = 1; i < n; i++)
     {
-        int aux = arr[i]; // Valor do elemento atual
-        int j = i - 1;    // Elemento anterior ao atual
+        int aux = arr[i];
+        int j = i - 1;
 
         while (j >= 0 && arr[j] > aux)
         {
-            arr[j + 1] = arr[j]; // Move o elemento anterior para a direita
-            j--;                 // Move para o próximo elemento anterior
+            arr[j + 1] = arr[j];
+            j--;
         }
-        arr[j + 1] = aux; // Move aux
+        arr[j + 1] = aux;
     }
     return arr;
 }

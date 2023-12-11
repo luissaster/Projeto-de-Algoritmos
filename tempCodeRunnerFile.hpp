278f000b2a0ef1aed0213234795633bd05501d46
Luís Fernando Almeida <luisfernalme@gmail.com>
2023-12-11 18:39:53 -0300

    heap sort

--- tempCodeRunnerFile.hpp
+++
@@ -1,3 +0,0 @@
-"Insertion Sort", "Bubble Sort", "Selection Sort",
-                                               "Shell Sort", "Merge Sort", "Quick Sort(First)", "Quick Sort(Average)",
-                                               "Quick Sort(Median of three)", "Quick Sort(Random)", "Heap Sort (min heap)"
